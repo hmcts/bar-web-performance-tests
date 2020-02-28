e300315e54c5ef2814190b96a93d0fd744fcbef9
kjain <Kapil.Jain@HMCTS.NET>
2020-02-28 11:44:20 +0000

    Scenario Update

--- src/gatling/simulations/uk/gov/hmcts/bar/simulation/BARPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/bar/simulation/BARPTSimulation.scala
@@ -15,23 +15,55 @@ class BARPTSimulation extends Simulation {
 
 
   val SCN = scenario("User Journey")
-                      .exec(
-                        BARLogout.Logout,
-                        LandingBARLoginPage.barLoginPage,
-                        SubmitLogin.loginAsFC,
-                        FCAddPayment.addPayment,
-                        FCPaymentList.goToPaymentList,
-                        FCPaymentList.selectPaymentID,
-                        FCPaymentList.searchFees,
-                        FCPaymentList.selectAndAddFees,
-                        FCPaymentList.processPayment,
-                        FCSubmitPayment.goToCheckAndSubmit,
-                        FCSubmitPayment.submitPayment,
-                        BARLogout.Logout
-                      )
+      .exec(
+      BARLogout.Logout,
+      LandingBARLoginPage.barLoginPage,
+      SubmitLogin.loginAsFC,
+      FCAddPayment.addPayment,
+      FCPaymentList.goToPaymentList,
+      FCPaymentList.selectPaymentID,
+      FCPaymentList.searchFees,
+      FCPaymentList.selectAndAddFees,
+      FCPaymentList.processPayment,
+      FCSubmitPayment.goToCheckAndSubmit,
+      FCSubmitPayment.submitPayment,
+      BARLogout.Logout
+    )
 
 
-  setUp(SCN.inject(rampUsers(1) during (1 minutes))
+  val SFCSCN = scenario("Senior FEE Clerk User Journey")
+
+    .exec(
+      BARLogout.Logout,
+      LandingBARLoginPage.barLoginPage,
+      SubmitLogin.loginAsSFC,
+      SFCActions.selectFeeClerk,
+      SFCActions.processPayment,
+      SFCActions.SFCViewPayment,
+      SFCActions.SFCSubmitPayment,
+      BARLogout.Logout
+    )
+
+
+  val DMSCN = scenario("DM User Journey")
+    .exec(
+      BARLogout.Logout,
+      LandingBARLoginPage.barLoginPage,
+      SubmitLogin.loginAsDM,
+      DMActions.selectSeniorFeeClerk,
+      DMActions.DMProcessPayment,
+      DMActions.DMViewPayment,
+      DMActions.DMubmitPayment,
+      DMPaymentList.DMNavigateToPaymentList,
+      DMTransferToBAR.transferToBAR,
+      BARLogout.Logout
+    )
+
+
+  setUp(SCN.inject(rampUsers(1) during (1 minutes)),
+    SFCSCN.inject(rampUsers(1) during (1 minutes)),
+    DMSCN.inject(rampUsers(1) during (1 minutes))
       ).protocols(httpProtocol)
+    .maxDuration(10 minute)
 
 }
