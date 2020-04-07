be3e8a4c064d00ac923cab967f2476a17fbabd81
DeepakGarcha <61466880+DeepakGarcha@users.noreply.github.com>
2020-04-07 14:58:11 +0100

    Updated Data File

--- src/gatling/simulations/uk/gov/hmcts/bar/simulation/BARPTSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/bar/simulation/BARPTSimulation.scala
@@ -1,21 +1,17 @@
 package uk.gov.hmcts.bar.simulation
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import uk.gov.hmcts.bar.scenarios._
 import uk.gov.hmcts.bar.scenarios.utils._
 import scala.concurrent.duration._
-
-class BARPTSimulation extends Simulation {
-
+class BARPTSimulation extends Simulation
+{
   val BashURL = Environment.baseURL
   val httpProtocol = http
     .baseUrl(BashURL)
     .proxy(Proxy("proxyout.reform.hmcts.net", 8080))
-
-
   val SCN = scenario("User Journey")
-      .exec(
+    .exec(
       BARLogout.Logout,
       LandingBARLoginPage.barLoginPage,
       SubmitLogin.loginAsFC,
@@ -29,10 +25,34 @@ class BARPTSimulation extends Simulation {
       FCSubmitPayment.submitPayment,
       BARLogout.Logout
     )
+  val SFCSCN = scenario("Senior FEE Clerk User Journey")
+    .exec(
+      BARLogout.Logout,
+      LandingBARLoginPage.barLoginPage,
+      SubmitLogin.loginAsSFC,
+      SFCActions.selectFeeClerk,
+      SFCActions.processPayment,
+      SFCActions.SFCViewPayment,
+      SFCActions.SFCSubmitPayment,
+      BARLogout.Logout
+    )
+  val DMSCN = scenario("DM User Journey")
+    .exec(
+      BARLogout.Logout,
+      LandingBARLoginPage.barLoginPage,
+      SubmitLogin.loginAsDM,
+      DMActions.selectSeniorFeeClerk,
+      DMActions.DMProcessPayment,
+      DMActions.DMViewPayment,
+      DMActions.DMubmitPayment,
+      DMPaymentList.DMNavigateToPaymentList,
+      DMTransferToBAR.transferToBAR,
+      BARLogout.Logout
+    )
 
-
-  setUp(SCN.inject(rampUsers(1) during (1 minutes))
-      ).protocols(httpProtocol)
-    .maxDuration(10 minute)
-
+  setUp(SCN.inject(rampUsers(300) during (15 minutes)),
+    SFCSCN.inject(nothingFor(20 minutes), rampUsers(315) during (15 minutes)),
+    DMSCN.inject(nothingFor(40 minutes), rampUsers(300) during (15 minutes))
+  ) .protocols(httpProtocol)
+    .maxDuration(65 minute)
 }
